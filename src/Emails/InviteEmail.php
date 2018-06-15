233baf798c91e1de761219ef0e0ab417c909904e
Andrew Parnham <aparnham@gcdtech.com>
2018-06-15 14:15:58 +0100

    Fix invite email link.

--- src/Emails/InviteEmail.php
+++ src/Emails/InviteEmail.php
@@ -38,11 +38,11 @@ class InviteEmail extends Email
     {
         $landlordSettings = LandlordSettings::singleton();
 
-        $rd = base64_encode("/app/accounts/?i={$this->invite->InviteID}");
+        $rd = base64_encode("/app/accounts/?i={$this->invite->_id}");
 
         return <<<END
 <p>You've been invited to join us!</p>
-<p><a href="{$landlordSettings->publicWebsiteUrl}login/?rd={$rd}&amp;i={$this->invite->InviteID}">Accept the invitation</a></p>
+<p><a href="{$landlordSettings->publicWebsiteUrl}login/?rd={$rd}&amp;i={$this->invite->_id}">Accept the invitation</a></p>
 END;
 
     }
